d5f68a8697efdc96a144253d4fc2e2497aea6fc7
Ben Farrell <ben@benfarrell.com>
2023-12-06 15:44:16 -0800

    allow single use

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
-export * from './bodysegmentation-video.js';
-
-import { load, processFrame} from './tensorflow/bodysegmentation';
-
-export const BodySegmentation = {
-    load, processFrame
-}
+export * from './tensorflow/bodysegmentation';
